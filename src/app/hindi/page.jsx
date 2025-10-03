aea133c6c2cafd70992d3a77f8a5afaaeb1f1911
CodingOm1 <164727053+CodingOm1@users.noreply.github.com>
2025-10-03 17:14:18 +0530

    changesmore2

--- src/app/hindi/page.jsx
+++ src/app/hindi/page.jsx
@@ -268,7 +268,7 @@ export default function Page() {
               <option value="">अपना लक्ष्य चुनें</option>
               <option value="Weight Loss">वजन कम करना</option>
               <option value="Weight Gain">वजन बढ़ाना</option>
-              <option value="Weight Maintain">फिटनेस मे रहणा </option>
+              <option value="Weight Maintain">फिटनेस </option>
             </select>
             <ErrorMessage error={errors.goal} />
           </div>
